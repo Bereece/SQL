371a8aca490fcf8c9198c808b5d5978b86c21c11
Frida A <dafrireece@gmail.com>
2022-09-03 16:40:19 +0300

    Update sql-practice.com-medium.sql

--- sql-practice.com-medium.sql
+++ sql-practice.com-medium.sql
@@ -222,6 +222,88 @@ GROUP BY day_number
 ORDER BY number_of_admissions DESC
 
 
+-- Q.17 Show the patient_id, nursing_unit_id, room, and bed for patient_id 542's most recent admission_date.
+
+
+SELECT patient_id, nursing_unit_id, room, bed 
+FROM admissions
+WHERE patient_id = 542
+GROUP BY patient_id
+HAVING MAX(admission_date);
+
+-- Alternate solution
+
+
+SELECT
+  patient_id,
+  nursing_unit_id,
+  room,
+  bed
+FROM admissions
+WHERE patient_id = 542
+GROUP BY patient_id
+HAVING
+  admission_date = MAX(admission_date);
+
+SELECT
+  patient_id,
+  nursing_unit_id,
+  room,
+  bed
+FROM admissions
+WHERE
+  patient_id = '542'
+  AND admission_date = (
+    SELECT MAX(admission_date)
+    FROM admissions
+    WHERE patient_id = '542'
+  )
+
+SELECT
+  patient_id,
+  nursing_unit_id,
+  room,
+  bed
+FROM admissions
+WHERE patient_id = 542
+ORDER BY admission_date DESC
+LIMIT 1
+
+SELECT
+  patient_id,
+  nursing_unit_id,
+  room,
+  bed
+FROM admissions
+GROUP BY patient_id
+HAVING
+  patient_id = 542
+  AND max(admission_date)
+
+
+-- Q.18 Show the nursing_unit_id and count of admissions for each nursing_unit_id. Exclude the following nursing_unit_ids: 'CCU', 'OR', 'ICU', 'ER'.
+
+SELECT nursing_unit_id,count(*) AS admissions_count
+FROM admissions
+WHERE nursing_unit_id NOT IN('CCU', 'OR', 'ICU', 'ER')
+group by nursing_unit_id;
+
+-- Alternate solution
+
+SELECT
+  nursing_unit_id,
+  count(*) count_of_admissions
+FROM admissions
+GROUP BY nursing_unit_id
+HAVING
+  nursing_unit_id NOT IN ('CCU', 'OR', 'ICU', 'ER');
+
+
+-- Q.19 Show patient_id, attending_physician_id, and primary_diagnosis for admissions that match one of the two criteria:
+1. patient_id is an odd number and attending_physician_id is either 1, 5, or 19.
+2. attending_physician_id contains a 2 and the length of patient_id is 3 characters.
+
+
 
 
 
